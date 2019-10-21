dd8efb0b14192cb8220371f997dcbcb6f4cf96b2
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-21 15:49:01 -0400

    Spruced things up and added a custom cursor

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let winner = undefined;
+let gameOver = false;
 const board = [
     {
         square: 0,
@@ -42,9 +43,21 @@ const board = [
     }
 ]
 const squares = document.querySelectorAll(".square");
+for (let i = 0; i < 9; i++) {
+    squares[i].classList.add("redPointer");
+}
 const winningCombos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 let currentPlayer = 1
 let filled = 0;
+let redScore = 0;
+let blueScore = 0;
+let ties = 0;
+const redScoreText = document.querySelector('.redScore');
+const blueScoreText = document.querySelector('.blueScore');
+const tieScore = document.querySelector('.ties');
+redScoreText.innerText = "0";
+blueScoreText.innerText = "0";
+tieScore.innerText = "0";
 
 const messageArea = document.querySelector(".message");
 let message = "It's Player 1's turn";
@@ -52,36 +65,47 @@ messageArea.innerText = message;
 
 function winnerCheck() {
     for (let i = 0; i < 8; i++) {
-        if (board[winningCombos[i][0]].clicked === "red"
-            && board[winningCombos[i][1]].clicked === "red"
-            && board[winningCombos[i][2]].clicked === "red") {
+        if (board[winningCombos[i][0]].clicked === "red" && 
+            board[winningCombos[i][1]].clicked === "red" && 
+            board[winningCombos[i][2]].clicked === "red" && 
+            gameOver === false) {
             winner = "red";
             messageArea.classList.remove("blueText");
             messageArea.classList.add("redText");
             message = "Red Wins!";
             messageArea.innerText = message;
+            redScore += 1;
+            redScoreText.innerText = redScore;
+            gameOver = true;
         }
         if (board[winningCombos[i][0]].clicked === "blue" &&
             board[winningCombos[i][1]].clicked === "blue" &&
-            board[winningCombos[i][2]].clicked === "blue") {
+            board[winningCombos[i][2]].clicked === "blue" && 
+            gameOver === false) {
             winner = "blue";
             messageArea.classList.remove("redText")
             messageArea.classList.add("blueText")
             message = "Blue Wins!";
             messageArea.innerText = message;
+            blueScore += 1;
+            blueScoreText.innerText = blueScore;
+            gameOver = true;
         }
     }
 }
 
 function checkTie() {
     for (let i = 1; i <= 9; i++) {
-        if(filled === 9 && winner === undefined) {
+        if (filled === 9 && winner === undefined && gameOver === false) {
             winner = "It's a tie!"
             messageArea.classList.remove("redText")
             messageArea.classList.remove("blueText")
             messageArea.classList.add("blackText")
             message = "It's a tie!";
             messageArea.innerText = message;
+            ties += 1;
+            tieScore.innerText = ties;
+            gameOver = true;
         }
     }
 }
@@ -97,6 +121,11 @@ function resetGame() {
         messageArea.classList.add("redText");
         message = "It's Player 1's turn";
         messageArea.innerText = message;
+        gameOver = false;
+        for(let i = 0; i < 9;i++) {
+            squares[i].classList.remove("bluePointer");
+            squares[i].classList.add("redPointer");
+        }
     }
     winner = undefined;
 }
@@ -113,6 +142,10 @@ function resetGame() {
                 messageArea.classList.add("blueText")
                 message = "It's Player 2's turn";
                 messageArea.innerText = message;
+                for (let i = 0; i < 9; i++) {
+                    squares[i].classList.remove("redPointer");
+                    squares[i].classList.add("bluePointer");
+                }
 
             } else if (currentPlayer === 2 && board[i + 1].clicked === undefined && winner === undefined) {
                 this.classList.toggle("blue");
@@ -123,6 +156,10 @@ function resetGame() {
                 messageArea.classList.add("redText");
                 message = "It's Player 1's turn";
                 messageArea.innerText = message;
+                for (let i = 0; i < 9; i++) {
+                    squares[i].classList.remove("bluePointer");
+                    squares[i].classList.add("redPointer");
+                }
             }
             winnerCheck()
             checkTie()
