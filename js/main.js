949c8841419fa10ed8b65ff961e7dae988d98d20
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-21 14:54:30 -0400

    Added more logic and tied it into HTML

--- js/main.js
+++ js/main.js
@@ -45,8 +45,9 @@ const squares = document.querySelectorAll(".square");
 const winningCombos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 let currentPlayer = 1
 let filled = 0;
+
 const messageArea = document.querySelector(".message");
-let message = "Player 1 is red and Player 2 is blue.\nIt's Player 1's turn";
+let message = "It's Player 1's turn";
 messageArea.innerText = message;
 
 function winnerCheck() {
@@ -55,11 +56,19 @@ function winnerCheck() {
             && board[winningCombos[i][1]].clicked === "red"
             && board[winningCombos[i][2]].clicked === "red") {
             winner = "red";
+            messageArea.classList.remove("blueText");
+            messageArea.classList.add("redText");
+            message = "Red Wins!";
+            messageArea.innerText = message;
         }
         if (board[winningCombos[i][0]].clicked === "blue" &&
             board[winningCombos[i][1]].clicked === "blue" &&
             board[winningCombos[i][2]].clicked === "blue") {
             winner = "blue";
+            messageArea.classList.remove("redText")
+            messageArea.classList.add("blueText")
+            message = "Blue Wins!";
+            messageArea.innerText = message;
         }
     }
 }
@@ -68,16 +77,26 @@ function checkTie() {
     for (let i = 1; i <= 9; i++) {
         if(filled === 9 && winner === undefined) {
             winner = "It's a tie!"
+            messageArea.classList.remove("redText")
+            messageArea.classList.remove("blueText")
+            messageArea.classList.add("blackText")
+            message = "It's a tie!";
+            messageArea.innerText = message;
         }
     }
 }
 
+document.querySelector('.reset').addEventListener('click', resetGame);
 function resetGame() {
     for (let i = 1; i <= 9; i++) {
         board[i].clicked = undefined;
         filled = 0;
         squares[i - 1].classList.remove("blue");
         squares[i - 1].classList.remove("red");
+        messageArea.classList.remove("blueText");
+        messageArea.classList.add("redText");
+        message = "It's Player 1's turn";
+        messageArea.innerText = message;
     }
     winner = undefined;
 }
@@ -90,11 +109,20 @@ function resetGame() {
                 board[i+1].clicked = "red"
                 filled += 1;
                 currentPlayer = 2;
+                messageArea.classList.remove("redText")
+                messageArea.classList.add("blueText")
+                message = "It's Player 2's turn";
+                messageArea.innerText = message;
+
             } else if (currentPlayer === 2 && board[i + 1].clicked === undefined && winner === undefined) {
                 this.classList.toggle("blue");
                 board[i + 1].clicked = "blue";
                 filled += 1;
                 currentPlayer = 1;
+                messageArea.classList.remove("blueText");
+                messageArea.classList.add("redText");
+                message = "It's Player 1's turn";
+                messageArea.innerText = message;
             }
             winnerCheck()
             checkTie()
