9e70b71c801893c48cc1e6ed1e319adbd8e751be
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-21 11:38:09 -0400

    created basic game logic

---
+++ js/main.js
@@ -0,0 +1,73 @@
+let winner = undefined;
+const board = [
+    {
+        square: 0,
+        clicked: undefined
+    },
+    {
+        square: 1,
+        clicked: undefined
+    },
+    {
+        square: 2,
+        clicked: "undefined"
+    },
+    {
+        square: 3,
+        clicked: undefined
+    },
+    {
+        square: 4,
+        clicked: undefined
+    },
+    {
+        square: 5,
+        clicked: undefined
+    },
+    {
+        square: 6,
+        clicked: undefined
+    },
+    {
+        square: 7,
+        clicked: undefined
+    },
+    {
+        square: 8,
+        clicked: undefined
+    },
+    {
+        square: 9,
+        clicked: undefined
+    }
+]
+const winningCombos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+
+function winnerCheck() {
+    for (let i = 0; i < 8; i++) {
+        if (board[winningCombos[i][0]].clicked === "red"
+            && board[winningCombos[i][1]].clicked === "red"
+            && board[winningCombos[i][2]].clicked === "red") {
+            winner = "red";
+        }
+        if (board[winningCombos[i][0]].clicked === "blue" &&
+            board[winningCombos[i][1]].clicked === "blue" &&
+            board[winningCombos[i][2]].clicked === "blue") {
+            winner = "blue";
+        }
+    }
+}
+
+function checkTie() {
+    for (let i = 1; i <= 9; i++) {
+        if(board[i].clicked !== undefined && winner === undefined) {
+            winner = "It's a tie!"
+        }
+    }
+}
+
+function resetGame() {
+    for (let i = 1; i <= 9; i++) {
+        board[i].clicked = undefined;
+    }
+}
